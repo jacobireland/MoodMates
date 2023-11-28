8e601e1d8f1c3dc22ecdd792d6647752838982f4
Jacob Ireland <jacobireland01@gmail.com>
2023-11-28 18:05:44 -0500

    almost done with static code for proposal popup

--- AwesomeProject/components/proposalPopup.js
+++ AwesomeProject/components/proposalPopup.js
@@ -1,16 +1,60 @@
-import React from 'react';
-import { TextInput, View, Button} from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, TextInput } from 'react-native';
 import styles from './proposalPopupStyles';
+import { BlurView } from 'expo-blur';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const ProposalPopup = () => {
+
+	//variable 'time' stores the time the user selects
+	// use time.toLocaleTimeString() to convert it to readable string
+	const [time, setTime] = useState(new Date());
+
+	const onChange = (event, selectedTime) => {
+		if (event.type === 'set') {
+		// User selected a date
+		setTime(selectedTime);
+		}
+	};		
+
 	return (
-		<View style={styles.mainContainer}>
+		<BlurView intensity={10} style={styles.mainContainer}>
 			<View style={styles.proPopupContainer}>
-				<View style={styles.modalContent}>
+				<View style={styles.popupContent}>
+					<Text style={styles.header}>Propose Activity</Text>
+					<View style={styles.containers}>
+						<Text style={styles.text}>Activity</Text>
+						<View style={styles.textView}>
+							<TextInput style={styles.input}/>
+						</View>
+					</View>
+					<View style={styles.containers}>
+						<Text style={styles.text}>Time</Text>
+						<View style={styles.timetextView}>
+							<DateTimePicker style={styles.dateTime}
+							mode="time" value={time}
+							onChange={onChange} />
+						</View>
+					</View>
+					<View style={styles.containers}>
+						<Text style={styles.text}>Location</Text>
+						<View style={styles.loctextView}>
+							<TextInput style={styles.input}/>
+						</View>
+					</View>
+					<View style={styles.notesContainer}>
+						<Text style={styles.text}>Notes</Text>
+						<View style={styles.notestextView}>
+							<TextInput multiline style={styles.notesInput} />
+						</View>
+					</View>
+					<View style={styles.sendProposal}>
+						<Text>Send Proposal</Text>
+					</View>
 				</View>
 			</View>
 
-		</View>
+		</BlurView>
 
 )}
 
--- AwesomeProject/components/proposalPopupStyles.js
+++ AwesomeProject/components/proposalPopupStyles.js
@@ -3,16 +3,92 @@ import {StyleSheet} from 'react-native'
 const styles = StyleSheet.create({
 	mainContainer: {
 		flex: 1,
-		justifyContent: 'center'
+		justifyContent: 'center',
+		justifyContent: 'center',
+		alignItems: 'center',
 	},
 
 	proPopupContainer: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
+		width: '100%',
+		height: 500,
 		backgroundColor: 'white',
 		borderRadius: 5,
 	},
+
+	popupContent: {
+		flex: 1,
+		padding: 25,
+		paddingTop: 15
+	},
+
+	header: {
+		fontSize: 30,
+		marginBottom: 35
+	},
+
+	text: {
+		fontSize: 18
+	},
+
+	containers: {
+		flexDirection: 'row',
+		height: 50,
+		alignItems: 'center',
+		marginBottom: 13,
+		width: '100%',
+	},
+
+	textView: {
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 4,
+		height: 40,
+		flex: 1,
+		justifyContent: 'center',
+		padding: 4,
+		marginLeft: 30
+	},
+
+	loctextView: {
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 4,
+		height: 40,
+		flex: 1,
+		justifyContent: 'center',
+		padding: 4,
+		marginLeft: 21
+	},
+
+	timetextView: {
+		height: 40,
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'flex-start',
+		marginLeft: 39
+	},
+
+	notesContainer: {
+		flexDirection: 'row',
+		height: 100,
+		alignItems: 'stretch',
+		marginBottom: 50,
+		width: '100%',
+	},
+
+	notestextView: {
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 4,
+		height: 40,
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'flex-start',
+		marginLeft: 41,
+		padding: 4,
+		height: '100%'
+	},
+
 
 
 })
