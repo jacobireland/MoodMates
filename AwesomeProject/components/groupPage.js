ddc75d89d3a1b70c9882ec7587c625a7be8dba23
ben kuhn <benkuhn@Bens-MacBook-Air-3.local>
2023-11-29 00:10:51 -0500

    setting up file structure :)

---
+++ AwesomeProject/components/groupPage.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import { Text, View, Image, TextInput } from 'react-native'; 
+import groupStyles from './groupPageStyle.js';
+
+const GroupPage = () => {
+
+return (
+	<View></View>
+)}
+export default GroupPage;
---
+++ AwesomeProject/components/groupPageStyle.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native'
+
+const groupStyles = StyleSheet.create({
+    
+
+
+
+
+
+
+ })
+
+export default groupStyles;
