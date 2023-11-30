54f81ab0b2f7fbe7d0b522cfee5d780f1a81fcf7
Jacob Ireland <jacobireland01@gmail.com>
2023-11-29 23:09:39 -0500

    finished profile page

--- AwesomeProject/components/profilePage.js
+++ AwesomeProject/components/profilePage.js
@@ -14,8 +14,8 @@ import anxiety5 from '../assets/anxiety6.png';
 import anxiety6 from '../assets/anxiety7.png';
 import anxiety7 from '../assets/anxiety8.png';
 import anxiety8 from '../assets/anxiety9.png';
-import backArrow from '../assets/backArrow.png';
 import settingsIcon from '../assets/settingsIcon.png';
+import backArrow from '../assets/backArrow.png';
 
 const ProfilePage = () => {
 
@@ -29,7 +29,42 @@ const ProfilePage = () => {
 				<Image
 				style={styles.settingsIcon}
 				source={settingsIcon}/>
+			</View>
 
+			<View style={styles.moodContainer}>
+				<MoodIcon size={100} happiness={'#FFD3B6'} energy={.9} anxiety={anxiety7}/>
+				<Text style={styles.moodText}>Your Current Mood</Text>
+			</View>
+
+			<View style={styles.friendsButton}>
+				<Text style={styles.friendsText}>Find Friends</Text>
+			</View>
+
+			<View style={styles.statsContainer}>
+				<View style={styles.friendsStat}>
+					<Text style={styles.fStatsText}>9</Text>
+					<Text style={styles.fStatsText}>Friends</Text>
+				</View>
+				<View style={styles.groupsStat}>
+					<Text style={styles.fStatsText}>3</Text>
+					<Text style={styles.fStatsText}>Groups</Text>
+				</View>
+			</View>
+
+			<View style={styles.trendsContainer}>
+				<Text style={styles.trendsText}>Your Mood Trends</Text>
+				<View style={styles.intervals}>
+					<Text style={styles.thisTexts}>This Week</Text>
+					<MoodIcon style={styles.moods} size={80} happiness={'#CFEDC0'} energy={.9} anxiety={anxiety7}/>
+				</View>
+				<View style={styles.intervals}>
+					<Text style={styles.thisTexts}>This Month</Text>
+					<MoodIcon style={styles.moods} size={80} happiness={'#FFD3B6'} energy={.75} anxiety={anxiety2}/>
+				</View>
+				<View style={styles.intervals}>
+					<Text style={styles.thisTexts}>This Year</Text>
+					<MoodIcon style={styles.moods} size={80} happiness={'#FFEBB5'} energy={1} anxiety={anxiety5}/>
+				</View>
 			</View>
 
 
--- AwesomeProject/components/profilePageStyles.js
+++ AwesomeProject/components/profilePageStyles.js
@@ -9,24 +9,97 @@ const styles = StyleSheet.create({
 
 	headingContainer: {
 		flexDirection: 'row',
-		alignItems: 'center'
+		alignItems: 'center',
+		marginBottom: 50
 	},
 
 	backArrow: {
-		width: 38,
-		height: 38
+		width: 27,
+		height: 27,
 	},
 
 	name: {
 		fontSize: 35,
-		marginLeft: 100,
-		marginRight: 100
+		margin: 88,
+		marginBottom: 0,
+		marginTop: 0
 	},
 
 	settingsIcon: {
 		width: 32,
-		height: 32
-	}
+		height: 32,
+	},
+	
+	moodContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginBottom: 40
+	},
+
+	moodText: {
+		fontSize: 18,
+		marginTop: 10
+	},
+
+	friendsButton: {
+		backgroundColor: '#8BC7AF',
+		width: 120,
+		height: 35,
+		borderRadius: 8,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+
+	statsContainer: {
+		width: '100%',
+		alignItems: 'flex-start',
+		justifyContent: 'flex-start',
+		flexDirection: 'row',
+		marginTop: 40
+	},
+
+	friendsStat: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 40
+	},
+
+	groupsStat: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 145
+	},
+
+	fStatsText: {
+		fontSize: 20,
+		marginBottom: 8
+	},
+
+	trendsContainer: {
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+
+	trendsText: {
+		fontSize: 20,
+		marginTop: 25,
+		marginBottom: 25
+	},
+
+	intervals: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+
+	thisTexts: {
+		fontSize: 17,
+	},
+
+
+
+
+
 	
 })
 
